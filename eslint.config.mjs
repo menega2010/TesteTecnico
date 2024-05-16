90a81dd3ddcb82c68615dcd168d7ba8934b03bc9
menega2010 <menega2010@gmail.com>
2024-05-16 20:15:58 -0300

    Configuracao eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+
+const config = {
+  // Defina o ambiente (node, browser, es2021)
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  // Extensões recomendadas e plugins
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  // Plugins adicionais
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  // Regras personalizadas
+  rules: {
+    'prettier/prettier': 'error', // Integração com Prettier
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/no-unresolved': 'error',
+  },
+  // Configurações do parser
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
+
+export default config;
